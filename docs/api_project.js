8e5d3afc784fc05dd2a6718e2a7a8d1ac989c34f
Jose Paternina <jlpaternina.ext@bacardi.com>
2023-04-11 16:50:00 +0200

    Add existing project files to Github

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "wines-api",  "version": "1.0.0",  "description": "Wines API",  "title": "Wines API",  "url": "https://wines-api.herokuapp.com/api",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-10-07T09:48:38.314Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
